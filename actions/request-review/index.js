41608482526f64b66184925af5141b0038195ae1
jakub <jplatek@purestorage.com>
2024-11-24 15:44:10 +0100

    test basic octokit call

---
+++ actions/request-review/index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  const token = core.getInput('github-token', {required: true});
+  const octokit = github.getOctokit(token);
+
+  const { data: pullRequest } = await octokit.rest.pulls.list({
+    owner: github.context.owner,
+    repo: github.context.repo
+  });
+
+  console.log(pullRequest);
+}
+
+run();
