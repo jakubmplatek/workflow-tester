7916a79d79c5d29b4bff4d144b512129c84c1cba
jakub <jplatek@purestorage.com>
2024-11-25 13:07:11 +0100

    debug

--- actions/request-review/index.js
+++ actions/request-review/index.js
@@ -5,11 +5,12 @@ async function run() {
   const token = core.getInput('github-token', {required: true});
   const octokit = github.getOctokit(token);
 
-  const { data: pullRequest } = await octokit.rest.pulls.list({
-    ...github.context.repo
-  });
+  const labels = ['devops', 'demo'];
 
-  console.log(pullRequest);
+  await github.rest.issues.addLabels({
+    ...github.context.repo,
+    labels: labels
+  });
 } 
 
 run();
